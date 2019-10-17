40ebb6852171a3a8bdb7e07f9345948508c81c04
gihanjayakuru <56676369+gihanjayakuru@users.noreply.github.com>
2019-10-17 14:57:57 +0530

    Update TestStudent.java

--- TestStudent.java
+++ TestStudent.java
@@ -16,5 +16,12 @@ class Student{
 		System.out.println(rollno+" "+name+" "+fee);
 	}
 
-	
+class TestStudent{
+	public static void main(String args[]){
+		Student S1=  new Student(111,"ankit","java");
+		Student S2=  new Student(112,"sumit","java",6000f);	
+		S1.display();
+		S2.display();
+	}
+}		
 	
