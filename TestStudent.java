59d5bf1cc74c5540d237352e3344269d9d6acc95
SandaruwanMherath <55899103+SandaruwanMherath@users.noreply.github.com>
2019-10-17 14:36:59 +0530

    Update TestStudent.java

--- TestStudent.java
+++ TestStudent.java
@@ -3,6 +3,12 @@ class Student{
 	String name,course;
 	float fee;
 	
+		Student(int rollno,String name,String course){
+		this.rollno=rollno;
+		this.name=name;
+		this.course=course;
+	}
+	
 
 	
 	
